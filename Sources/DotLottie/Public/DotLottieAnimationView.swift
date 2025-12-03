102e02c90a27c63c80d4ae9452d6308926f01fad
Kolya <npalto2139@yandex.ru>
2025-12-03 13:01:57 +0300

    Update DotLottieAnimationView.swift make public init

--- Sources/DotLottie/Public/DotLottieAnimationView.swift
+++ Sources/DotLottie/Public/DotLottieAnimationView.swift
@@ -31,7 +31,7 @@ open class DotLottieAnimationView: UIView, DotLottie {
         setupMetalView()
     }
     
-    required init?(coder aDecoder: NSCoder) {
+    public required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
